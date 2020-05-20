425a7a62d446d6896b9f884a4c5a6507848b07a4
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-20 19:30:39 +0200

    express-validator added. user model

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/UserModel");
 
 //@route    POST  api/users
 //@desc     Register a User
 //@access   Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email"),
+    check(
+      "password",
+      "Please enter a password with at least 6 characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 //Connect DB
 connectDB();
 
+//Init Middleware
+//By doing this we can accepta data. Using the req.body
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.json("Welcome to the Neat.ME API"));
 
 ///Define Routes
