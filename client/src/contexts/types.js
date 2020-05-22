59ce0d72a110584c171d9a1f55a978eb6b168483
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-22 02:22:14 +0200

    show users on ui add contact form working iwth the hardcoded users

--- client/src/contexts/types.js
+++ client/src/contexts/types.js
@@ -1,4 +1,4 @@
-export const ADD_CONTACTs = "ADD_CONTACTs";
+export const ADD_CONTACT = "ADD_CONTACT";
 export const DELETE_CONTACT = "DELETE_CONTACT";
 export const SET_CURRENT = "SET_CURRENT";
 export const CLEAR_CURRENT = "CLEAR_CURRENT";
