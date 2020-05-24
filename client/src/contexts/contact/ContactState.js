6013a3e3d963651fc5a7e8100bfd84c5f67e6ede
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-24 21:55:32 +0200

    update works. full CRUD works

--- client/src/contexts/contact/ContactState.js
+++ client/src/contexts/contact/ContactState.js
@@ -57,8 +57,42 @@ const ContactState = (props) => {
   };
 
   //Delete Contact
-  const deleteContact = (id) => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const deleteContact = async (id) => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+
+      dispatch({ type: DELETE_CONTACT, payload: id });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
+  };
+
+  //Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
+
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //Clear Contacts
@@ -74,11 +108,6 @@ const ContactState = (props) => {
   //Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  //Update Contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //Filter Contacts
