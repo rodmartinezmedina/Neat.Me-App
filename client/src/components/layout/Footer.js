47f8505d26811b2d6387bb22580230dbd06d4dce
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-06-10 15:54:26 +0200

    .

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,33 +2,44 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const Footer = ({ iconGithub, iconLinkedin }) => {
+const Footer = ({ iconGithub, iconLinkedin, iconWebsite }) => {
   return (
     <div>
       <nav className="footer bg-secondary">
         <ul>
           <li id="footer-li">
-            <i className={iconGithub} /> Visit my Github
             <a
               id="footer-text"
               href="https://github.com/rodmartinezmedina"
               target="_blank"
               rel="noopener noreferrer"
-            ></a>
+            >
+              <i className={iconGithub} /> Visit my Github
+            </a>
           </li>
           <li id="footer-li">
-            <i className={iconLinkedin} /> Contact me on Linkedin
             <a
               id="footer-text"
               href="https://www.linkedin.com/in/rodrigo-martinez-medina/"
               target="_blank"
               rel="noopener noreferrer"
-            ></a>
+            >
+              <i className={iconLinkedin} /> Contact me on Linkedin
+            </a>
           </li>
           <li id="footer-li">
             <Link to="/about">About Me & Neat.ME</Link>
           </li>
-          <li id="footer-li">@rodmartinezmedina</li>
+          <li id="footer-li">
+            <a
+              id="footer-text"
+              href="https://rodmartinezmedina.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className={iconWebsite} /> My personal Website
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
@@ -38,11 +49,13 @@ const Footer = ({ iconGithub, iconLinkedin }) => {
 Footer.defaultProps = {
   iconGithub: "fab fa-github",
   iconLinkedin: "fab fa-linkedin",
+  iconWebsite: "fas fa-globe",
 };
 
 Footer.propTypes = {
   iconGithub: PropTypes.string.isRequired,
   iconLinkedin: PropTypes.string.isRequired,
+  iconWebsite: PropTypes.string.isRequired,
 };
 
 export default Footer;
